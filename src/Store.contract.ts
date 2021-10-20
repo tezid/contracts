ebedf4bc0481e44ebdd9749adde682cdd887ff74
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-10-20 13:25:11 +0200

    Added farm + stor compiling

--- src/Store.contract.ts
+++ src/Store.contract.ts
@@ -2,15 +2,12 @@ import * as Types from './Types';
 
 type TStoreStorage = TRecord<{
   admin: TAddress;
-  identities: Types.TIdentities
+  identities: Types.TIdentities;
 }> 
 
 @Contract
 export class TezIDStore {
-  constructor(public storage: TStoreStorage) {
-    admin = storage.admin
-    identities = storage.identities
-  }
+  constructor(public storage: TStoreStorage) {}
 
   @EntryPoint
   default(): void {
@@ -25,58 +22,67 @@ export class TezIDStore {
     this.storage.admin = new_admin
   }
 
-  @EntryPoint
-  setBaker(new_delegate: TAddress): void {
-    if (Sp.sender != this.storage.admin) {
-      Sp.failWith("Only admin can setBaker")
-    }
-    Sp.setDelegate(new_delegate)
-  }
-      
-  @EntryPoint
-  send(receiverAddress: TAddress, amount: TMutez): void {
-    if (Sp.sender != this.storage.admin) {
-      Sp.failWith("Only admin can send")
-    }
-    const contract: TContract<TUnit> = Sp.contract<TUnit>(receiverAddress, "").openSome("Invalid Interface");
-    Sp.transfer(Sp.unit, amount, contract)
-  }
-
-  @EntryPoint
-  setProof(address: TAddress, prooftype: TString, proof: Types.TProof): void {
-    if (Sp.sender != this.storage.admin) {
-      Sp.failWith("Only admin can setProof")
-    }
-    if (!this.storage.identities.contains(address)) {
-      this.storage.identities.set(address, {})
-    }
-    this.storage.identities.get(address).set(prooftype, proof)
-  }
-      
-  @EntryPoint
-  delProof(address: TAddress, prooftype: TString): void {
-    if (Sp.sender != this.storage.admin) {
-      Sp.failWith("Only admin can delProof")
-    }
-    this.storage.identities.get(address).remove(prooftype)
-  }
-      
-  @EntryPoint
-  removeIdentity(address: TAddress): void {
-    if (Sp.sender != this.storage.admin) {
-      Sp.failWith("Only admin can removeIdentity")
-    }
-    this.storage.identities.remove(address)
-  }
-
-  @EntryPoint
-  getProofs(address: TAddress, callback_address: TContract): void {
-    let proofs = {}
-    if (this.storage.identities.contains(address)) {
-      proofs = this.storage.identities.get(address)
-    }
-    const res: Types.TGetProofsResponsePayload = { address = address, proofs = proofs }
-    const contract: TContract<Types.TGetProofsResponsePayload> = Sp.contract<Types.TGetProofsResponsePayload>(callback_address).openSome("Invalid Interface");
-    Sp.transfer(res, 0, contract)
-  }
+//  @EntryPoint
+//  setBaker(new_delegate: TAddress): void {
+//    if (Sp.sender != this.storage.admin) {
+//      Sp.failWith("Only admin can setBaker")
+//    }
+//    Sp.setDelegate(new_delegate)
+//  }
+//      
+//  @EntryPoint
+//  send(receiverAddress: TAddress, amount: TMutez): void {
+//    if (Sp.sender != this.storage.admin) {
+//      Sp.failWith("Only admin can send")
+//    }
+//    const contract: TContract<TUnit> = Sp.contract<TUnit>(receiverAddress, "").openSome("Invalid Interface");
+//    Sp.transfer(Sp.unit, amount, contract)
+//  }
+//
+//  @EntryPoint
+//  setProof(address: TAddress, prooftype: TString, proof: Types.TProof): void {
+//    if (Sp.sender != this.storage.admin) {
+//      Sp.failWith("Only admin can setProof")
+//    }
+//    if (!this.storage.identities.contains(address)) {
+//      this.storage.identities.set(address, {})
+//    }
+//    this.storage.identities.get(address).set(prooftype, proof)
+//  }
+//      
+//  @EntryPoint
+//  delProof(address: TAddress, prooftype: TString): void {
+//    if (Sp.sender != this.storage.admin) {
+//      Sp.failWith("Only admin can delProof")
+//    }
+//    this.storage.identities.get(address).remove(prooftype)
+//  }
+//      
+//  @EntryPoint
+//  removeIdentity(address: TAddress): void {
+//    if (Sp.sender != this.storage.admin) {
+//      Sp.failWith("Only admin can removeIdentity")
+//    }
+//    this.storage.identities.remove(address)
+//  }
+//
+//  @EntryPoint
+//  getProofs(address: TAddress, callback_address: TContract): void {
+//    let proofs = {}
+//    if (this.storage.identities.contains(address)) {
+//      proofs = this.storage.identities.get(address)
+//    }
+//    const res: Types.TGetProofsResponsePayload = { address = address, proofs = proofs }
+//    const contract: TContract<Types.TGetProofsResponsePayload> = Sp.contract<Types.TGetProofsResponsePayload>(callback_address).openSome("Invalid Interface");
+//    Sp.transfer(res, 0, contract)
+//  }
 }
+
+Dev.test({ name: 'Store origination' }, () => {
+  const admin = Scenario.testAccount("Admin")
+  const store = Scenario.originate(new TezIDStore({
+    admin: admin.address,
+    identities: [] 
+  }))
+  Scenario.verify(store.storage.admin == admin.address)
+})
