c5c1de7da67a8a6b92323b73025aaab4c6ad33d5
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-12-10 10:42:50 +0100

    Updates

--- contracts/store.py
+++ contracts/store.py
@@ -1,35 +1,8 @@
 import smartpy as sp
 
-## Types
-#
+import Types from 
 
-TProof = sp.TRecord(
-    register_date = sp.TTimestamp,
-    verified = sp.TBool,
-    meta = sp.TMap(sp.TString, sp.TString)
-)
-TProofs = sp.TMap(sp.TString, TProof)
-TIdentities = sp.TBigMap(sp.TAddress, TProofs)
-TSendPayload = sp.TRecord(receiverAddress = sp.TAddress, amount = sp.TMutez)
-TSetProofPayload = sp.TRecord(
-    address=sp.TAddress,
-    prooftype=sp.TString,
-    proof=TProof
-)
-TDelProofPayload = sp.TRecord(
-    address=sp.TAddress,
-    prooftype=sp.TString
-)
-TGetProofsRequestPayload = sp.TRecord(
-    address=sp.TAddress, 
-    callback_address=sp.TAddress
-)
-TGetProofsResponsePayload = sp.TRecord(
-  address = sp.TAddress,
-  proofs = TProofs
-)
-
-## TezIDStorage
+## TezIDStore
 #
 
 class TezIDStore(sp.Contract):
---
+++ contracts/types.py
@@ -0,0 +1,30 @@
+import smartpy as sp
+
+## Types
+#
+
+TProof = sp.TRecord(
+    register_date = sp.TTimestamp,
+    verified = sp.TBool,
+    meta = sp.TMap(sp.TString, sp.TString)
+)
+TProofs = sp.TMap(sp.TString, TProof)
+TIdentities = sp.TBigMap(sp.TAddress, TProofs)
+TSendPayload = sp.TRecord(receiverAddress = sp.TAddress, amount = sp.TMutez)
+TSetProofPayload = sp.TRecord(
+    address=sp.TAddress,
+    prooftype=sp.TString,
+    proof=TProof
+)
+TDelProofPayload = sp.TRecord(
+    address=sp.TAddress,
+    prooftype=sp.TString
+)
+TGetProofsRequestPayload = sp.TRecord(
+    address=sp.TAddress, 
+    callback_address=sp.TAddress
+)
+TGetProofsResponsePayload = sp.TRecord(
+  address = sp.TAddress,
+  proofs = TProofs
+)
