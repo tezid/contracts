ef4120aa0fb7246828979595185aa0f7f6881713
Asbjørn Enge <asbjorn@hanafjedle.net>
2022-01-20 13:38:57 +0100

    XFarm initial commit

--- contracts/farm.py
+++ contracts/farm.py
@@ -25,7 +25,12 @@ class TezIDForeverFarm(sp.Contract):
       rewardToken = sp.record(
         address  = sp.none,
         token_id = 0
+      ),
+      daoToken = sp.record(
+        address  = sp.none,
+        token_id = 0
       )
+      burnAddress = sp.none
     )
 
   ## Utility Functions
@@ -55,17 +60,10 @@ class TezIDForeverFarm(sp.Contract):
       sp.transfer(arg, sp.mutez(0), transferHandle)
 
   @sp.private_lambda(with_storage='read-only', with_operations=True, wrap_call=True)
-  def calculateRewards(self, stakes):
-    rewards = sp.local('rewards', 0)
-    sp.for stakeDate in stakes:
-      sp.for rewardDate in self.data.rewards.keys():
-        sp.if stakeDate < rewardDate:
-          claimDate = self.data.stakers[sp.sender][stakeDate].claimed
-          sp.if claimDate < rewardDate:
-            amountStaked = self.data.stakers[sp.sender][stakeDate].amount
-            totalStaked = self.data.rewards[rewardDate]
-            rewards.value += totalStaked / amountStaked
-    sp.result(rewards.value)
+  def getTokenValue(self):
+    tokenValue = self.data.rewardPool / self.data.totalStaked  
+    sp.result(tokenValue)
+
 
   ## Checks
   #
@@ -132,10 +130,7 @@ class TezIDForeverFarm(sp.Contract):
       ids=[self.data.rewardToken.token_id],
       amount=amount
     ))
-    self.rewards[sp.now] = sp.record(
-      amount = amount,
-      totalStaked = self.data.totalStaked
-    )
+    self.data.rewardPool += amount
 
   ## Stake
   #
@@ -143,6 +138,12 @@ class TezIDForeverFarm(sp.Contract):
   @sp.entry_point
   def stake(self, amount):
     self.checkNotPaused()
+    self.data.totalStaked += amount
+    tokenValue = self.getTokenValue()
+    rewardTokenPaymentRequired = tokenValue * amount
+    self.data.rewardPool += rewardTokenPaymentRequired 
+
+    # Claim stakeTokens
     self.TransferTokens(sp.record(
       sender=sp.sender, 
       receiver=sp.self_address, 
@@ -150,43 +151,56 @@ class TezIDForeverFarm(sp.Contract):
       ids=[self.data.stakeToken.token_id],
       amount=amount
     ))
-    stakes = sp.lokal('stakes', sp.map({}))
-    sp.if self.data.stakers.contains(sp.sender):
-      stakes.value = self.data.stakers[sp.sender]
-    stakes.value[sp.now] = sp.record(
-      amount = amount,
-      claimed = sp.now
-    )
-    self.data.totalStaked += amount
+    # Return daoTokens
+    self.TransferTokens(sp.record(
+      sender=sp.self_address, 
+      receiver=sp.sender, 
+      token=self.data.daoToken.address.open_some('daoToken address not set'),
+      ids=[self.data.daoToken.token_id],
+      amount=amount
+    ))
+    # Claim rewardTokens
+    self.TransferTokens(sp.record(
+      sender=sp.sender, 
+      receiver=sp.self_address, 
+      token=self.data.rewardToken.address.open_some('rewardToken address not set'),
+      ids=[self.data.rewardToken.token_id],
+      amount=rewardTokenPaymentRequired
+    ))
 
-  ## Claim
+  ## Exit
   #
 
   @sp.entry_point
-  def claim(self, stakes):
+  def exit(self, amount):
     self.checkNotPaused()
-    rewards = self.calculateRewards(stakes)
-    sp.for stakeDate in stakes:
-      # Note: more secure to handle this in calculateRewards and use rewardDate
-      self.data.stakers[sp.sender][stakeDate].claimed = sp.now
+    self.data.totalStaked -= amount
+    tokenValue = self.getTokenValue()
+    rewardTokenPayment = tokenValue * amount
+    self.data.rewardPool -= rewardTokenPayment
+
+    # Return stakeToken
+    self.TransferTokens(sp.record(
+      sender=sp.self_address, 
+      receiver=sp.sender, 
+      token=self.data.stakeToken.address.open_some('stakeToken address not set'),
+      ids=[self.data.stakeToken.token_id],
+      amount=amount
+    ))
+    # Burn daoTokens
+    self.TransferTokens(sp.record(
+      sender=sp.sender, 
+      receiver=self.data.burnAddress.open_some('brunAddress not set'), 
+      token=self.data.daoToken.address.open_some('daoToken address not set'),
+      ids=[self.data.daoToken.token_id],
+      amount=amount
+    ))
+    # Return rewardTokens
     self.TransferTokens(sp.record(
       sender=sp.self_address, 
       receiver=sp.sender, 
       token=self.data.rewardToken.address.open_some('rewardToken address not set'),
       ids=[self.data.rewardToken.token_id],
-      amount=rewards.value
+      amount=rewardTokenPayment
     ))
 
-  @sp.entry_point
-  def unstake(self, stakes):
-    self.checkNotPaused()
-    rewards = self.calculateRewards(stakes)
-    sp.for stakeDate in stakes:
-      del self.data.stakers[sp.sender][stakeDate]
-    self.TransferTokens(sp.record(
-      sender=sp.self_address, 
-      receiver=sp.sender, 
-      token=self.data.rewardToken.address.open_some('rewardToken address not set'),
-      ids=[self.data.rewardToken.token_id],
-      amount=rewards.value
-    ))
